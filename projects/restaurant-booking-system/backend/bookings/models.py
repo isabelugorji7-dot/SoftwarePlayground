dd619472b32e53e8bd60adb5a4ba3b0ac414c684
160264749-hub <160264749@student.rnngroup.ac.uk>
2026-07-21 22:56:16 +0100

    Create first website structure

--- projects/restaurant-booking-system/backend/bookings/models.py
+++ projects/restaurant-booking-system/backend/bookings/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django import forms
+from .models import Booking
 
-# Create your models here.
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['name', 'email', 'date', 'time', 'guests']
